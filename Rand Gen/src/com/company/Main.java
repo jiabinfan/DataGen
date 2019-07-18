77afb594ad0f998e451f92408b12773cf6e4548d
Arthur <arthurerconggao@gmail.com>
2019-07-18 09:52:38 -0600

    w

--- Rand Gen/src/com/company/Main.java
+++ Rand Gen/src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
     //public static ArrayList<Integer> totSum = new ArrayList<>();
 
     public static String fileStructure = "/home/agao/ALL_DATA_12-1000/";
-    public static String inFile = "110_Cord.csv";
+    public static String inFile = "25_Cord.csv";
     public static int numGen = 1000;
 
     public static void main(String[] args) {

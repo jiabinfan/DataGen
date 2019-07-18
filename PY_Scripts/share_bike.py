25666a2cdf154d80da1d2bff870704fe5bc57c63
Arthur <arthurerconggao@gmail.com>
2019-07-18 14:32:58 -0600

    f

--- PY_Scripts/share_bike.py
+++ PY_Scripts/share_bike.py
@@ -10,10 +10,10 @@ FULL = "full"
 INFINIT = 9999999
 TRUCK_WEIGHT = 0
 
-data_path = "/home/agao/ALL_DATA_7-25/"
-rangeTop = 26
+data_path = "/home/agao/ALL_DATA_7-1000/"
+rangeTop = 1000
 truckFull = 15
-extraNum = 10
+extraNum = 0
 
 class Station:
     full_load = 20
